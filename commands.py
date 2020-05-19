b274529070296db2e3c5f088533fff9385cb8891
Dan's iMac Pro <dan@iMac-Pro.local>
2020-05-19 11:44:16 +0100

    added functions to commands file, clearing main.

---
+++ commands.py
@@ -0,0 +1,62 @@
+from secret_key import api_client_id
+from secret_key import auth_token
+from secret_key import login
+from secret_key import password
+
+import requests
+import pprint
+
+headers = {
+    'X-Client-Id': api_client_id,
+    'X-Auth-Token': auth_token,
+}
+
+pp = pprint.PrettyPrinter(indent=4)
+
+def list_videos(q=''):
+    '''This makes an API call to vzaar and lists all videos in account'''
+
+    response = requests.get('https://api.vzaar.com/api/v2/videos', headers=headers)
+
+    return response.text
+
+def list_categories():
+    '''This makes an API call to vzaar and lists all categories in account'''
+
+    response = requests.get('https://vzaar.com/api/v2/categories', headers=headers)
+
+    return response.text
+
+def list_category_subtree(id):
+    '''This makes an API call to vzaar and lists all categories in a given subtree'''
+
+    response = requests.get('https://vzaar.com/api/v2/categories/' + id + '/subtree', headers=headers)
+
+    return response.text
+
+def lookup_category(id):
+    '''This makes an API call to vzaar and looks up a category in a given id'''
+
+    response = requests.get('https://vzaar.com/api/v2/categories/' + id, headers=headers)
+
+    return response.text
+
+def create_top_level_category(name):
+    '''Use this to create a category on the top level of category lists.
+    Reserved for programmes and qualifications.
+    
+    name parameter must be a string.'''
+
+    auth_head = {
+        'X-Client-Id': api_client_id,
+        'X-Auth-Token': auth_token,
+        'Content-Type': 'application/json'
+    }
+
+    data = {
+        'name': name
+    }
+
+    response = requests.post('https://vzaar.com/api/v2/categories', headers=auth_head, data=data)
+    print(response.status_code)
+
