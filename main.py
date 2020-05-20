d6614a15f9bd7ba86af0fa227e06a5d01078dc0e
twentynineteen <21161833+twentynineteen@users.noreply.github.com>
2020-05-20 14:35:46 +0100

    scan through dictionary

--- main.py
+++ main.py
@@ -10,12 +10,19 @@ import json
 
 # create_top_level_category("test category")
 
-categories = list_categories()
-categories_obj = json.loads(categories)
-for key in categories_obj['data']:
+# categories = list_categories()
+# categories_obj = json.loads(categories)
+# listing = categories_obj['data'][0]
+
+# for key in listing:
+#     print(listing[key])
+
+
+# for key in categories_obj['data']:
     # print(categories_obj['data'][0]['id'])
-    # print(len(key))
-    print(len(key))
+    # print(key['id'])
+    # print(key['title'])
+
     # for i in len(range(key)):
     #     print(i + ':', value[i])
 
@@ -23,30 +30,39 @@ for key in categories_obj['data']:
 # create_subcategory("test sub", 10127)
 
 
-video_dict = {
-    "data":[
-        {
-            "id":21565695,
-            "title":"IB92X0 - Y Yang - Day 4_AI and Services - ' 'Machine ' 'Learning",
-            "description":"",
-            "state":"ready",
-            "private":False,
-            "user_id":106443,
-            "user_login":"elearning",
-            "account_id":80122,
-            "account_name":"WBS","duration":2001.2,
-            "created_at":"2020-05-20T10:01:47.000Z",
-            "updated_at":"2020-05-20T10:02:01.000Z",
-            "url":"https://vzaar.com/videos/21565695",
-            "asset_url":"https://view.vzaar.com/21565695/video",
-            "poster_url":"https://view.vzaar.com/21565695/image",
-            "thumbnail_url":"https://view.vzaar.com/21565695/thumb",
-            "categories":[],
-            "adverts":[]
-        }
-            ]
-            }
+# category_dict = {
+    # "data":[
+    #     {
+    #         'id': 10127, 
+    #         'account_id': 80122, 
+    #         'user_id': 106443, 
+    #         'name': 'Warwick', 
+    #         'description': None, 
+    #         'parent_id': None, 
+    #         'depth': 0, 
+    #         'node_children_count': 5, 
+    #         'tree_children_count': 11, 
+    #         'node_video_count': 0, 
+    #         'tree_video_count': 56, 
+    #         'created_at': '2017-09-19T08:42:13.000Z', 
+    #         'updated_at': '2020-05-19T11:46:24.000Z'}
+    #         ]
+    #         }
 
 
 # print(video_dict.items())
 # print(video_dict['data'][0]['id'])
+
+
+videos = list_videos()
+videos_obj = json.loads(videos)
+listing = videos_obj['data'][0]
+
+for list in listing:
+    print(str(list) + ' : \n' + str(listing[list]))
+
+
+
+
+# for key in listing:
+#     print(listing[key])
